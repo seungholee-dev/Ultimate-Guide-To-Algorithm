d900e44b9765cd5ce7e84977f5eb269fa3208a78
poream3387 <poream3387@gmail.com>
2020-07-17 22:54:42 +0900

    Fixed bug in Merge Sort code in the Algorithm Codes folder

--- Algorithm Codes/Sorting/merge_sort.py
+++ Algorithm Codes/Sorting/merge_sort.py
@@ -18,44 +18,51 @@ def merge(arr, l, m, r):
     if l == r:
         return
 
-    l_pointer = l
-    r_pointer = m + 1
+    l_pointer, r_pointer = l, m + 1
 
     tmp_arr = [0] * (r - l + 1)
     
-    # Comparison O(n) sort
+    # Comparing and sort
     tmp_pointer = 0
-    while l_pointer < m and r_pointer < r + 1:
+    while l_pointer < m + 1 and r_pointer < r + 1:
+        # print(l_pointer, ":", arr[l_pointer], r_pointer, ":", arr[r_pointer])
         if arr[l_pointer] < arr[r_pointer]:
-            tmp_pointer = arr[l_pointer]
+            tmp_arr[tmp_pointer] = arr[l_pointer]
             l_pointer += 1
         else:
-            tmp_pointer = arr[r_pointer]
+            tmp_arr[tmp_pointer] = arr[r_pointer]
             r_pointer += 1
         tmp_pointer += 1
-    
+
+
     # Put the rest from the left array (if there are any)
-    while l_pointer < m:
-        arr[tmp_pointer] = arr[l_pointer]
+    while l_pointer < m + 1:
+        tmp_arr[tmp_pointer] = arr[l_pointer]
         l_pointer += 1
-        tmp_pointer
+        tmp_pointer += 1
 
     # Put the rest from the  array (if there are any)
     while r_pointer < r + 1:
-        arr[tmp_pointer] = arr[r_pointer]
+        tmp_arr[tmp_pointer] = arr[r_pointer]
         r_pointer += 1
-        tmp_pointer
+        tmp_pointer += 1
 
     # Copy back to the original array
     for i in range(len(tmp_arr)):
         arr[l + i] = tmp_arr[i]
 
 if __name__ == "__main__":
-    arr = [7, 8, 0, 1, 2, 3, 8, 9, 4, 3, 4]
-    print(merge_sort(arr, 0, len(arr)))
+    arr = [7, 8, 0, 1, 2, 3, 8, 9, 4, 3, 4, 6, 7, 8, 9, 10, 16]
+    print(arr)
+    merge_sort(arr, 0, len(arr) - 1)
+    print(arr)
 
     arr = [2, 1]
-    print(merge_sort(arr, 0, len(arr)))
+    print(arr)
+    merge_sort(arr, 0, len(arr) - 1)
+    print(arr)
 
     arr = [1]
-    print(merge_sort(arr, 0, len(arr)))
+    print(arr)
+    merge_sort(arr, 0, len(arr) - 1)
+    print(arr)
