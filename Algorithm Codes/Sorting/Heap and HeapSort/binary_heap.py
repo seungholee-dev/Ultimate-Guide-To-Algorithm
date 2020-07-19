b07e18ce5bf9e2bb339cf8d91f34a001bfdd30d9
poream3387 <poream3387@gmail.com>
2020-07-19 22:26:45 +0900

    Quicksort fixing bugs and added heap

---
+++ Algorithm Codes/Sorting/Heap and HeapSort/binary_heap.py
@@ -0,0 +1,12 @@
+def build_max_heap(arr):
+    
+    return arr
+
+def max_heapify(arr):
+    pass
+
+
+if __name__ == "__main__":
+    arr = [7, 3, 1, 2, 8, 4, 6, 5, 10, 9]
+    min_heap(arr)
+    print(arr)
--- Algorithm Codes/Sorting/quick_sort.py
+++ Algorithm Codes/Sorting/quick_sort.py
@@ -1,8 +1,9 @@
 # Divide and conquer 
 def quick_sort(arr, l, r):
-    border = partition(arr, l, r) # Partition the array and get the pivot index
-    quick_sort(arr, l, border - 1) # quicksort l ~ (pivot - 1) 
-    quick_sort(arr, border + 1, r) # quicksort (pivot + 1) ~ r
+    if l < r:
+        border = partition(arr, l, r) # Partition the array and get the first index of the right segment
+        quick_sort(arr, l, border - 1) # quicksort left half: l ~ (border - 1) 
+        quick_sort(arr, border, r) # quicksort right half: (border) ~ r
 
 # Paritioning the left and right parts (based on the pivot)
 def partition(arr, l, r):
@@ -10,33 +11,34 @@ def partition(arr, l, r):
     l_pointer = l
     r_pointer = r
     
-    while l_pointer < 
-
-    # Pick one from the left
-    while arr[l_pointer] <= pivot_value:
-        l_pointer += 1
-    
-    # Pick one from the right
-    while arr[r_pointer] >= pivot_value:
-        r_pointer -= 1
+    while l_pointer < r_pointer:
+        # Pick one from the left (That is larger than or equal to the pivot_value)
+        while arr[l_pointer] < pivot_value and l_pointer < len(arr) - 1:
+            l_pointer += 1
         
-    # Swap two
-    arr[l_pointer], arr[r_pointer] = arr[r_pointer], arr[l_pointer]
-    
-    
-
+        # Pick one from the right (That is smaller than or equal to the pivot_value)
+        while arr[r_pointer] > pivot_value and r_pointer > 0:
+            r_pointer -= 1
+            
+        # Swap the two
+        if l_pointer < r_pointer:
+            arr[l_pointer], arr[r_pointer] = arr[r_pointer], arr[l_pointer]
+            l_pointer += 1
+            r_pointer -= 1
+        
+    # Return the first element of the right half (border)
     return l_pointer
 
 if __name__ == "__main__":
-    # arr = [7, 8, 0, 1, 2, 3, 8, 9, 4, 3, 4, 6, 7, 8, 9, 10, 16]
-    # print(arr)
-    # quick_sort(arr, 0, len(arr) - 1)
-    # print(arr)
+    arr = [2, 1, 4]
+    print(arr)
+    quick_sort(arr, 0, len(arr) - 1)
+    print(arr)
 
-    # arr = [2, 1]
-    # print(arr)
-    # quick_sort(arr, 0, len(arr) - 1)
-    # print(arr)
+    arr = [1, 4, 8, 1, 3, 4, 0, 3, 9, 4, 2, 3, 1, 9]
+    print(arr)
+    quick_sort(arr, 0, len(arr) - 1)
+    print(arr)
 
     # arr = [1]
     # print(arr)
