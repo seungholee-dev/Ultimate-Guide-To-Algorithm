dbe051f0c8f45ec46eb4a4dba9e188554e9bfdd1
Nananaw <gamingnananaw@gmail.com>
2023-06-19 10:08:18 -0400

    Fixed Dijkstra's algorithm and added example test case (now gives, the shortest path to each node from source)

--- Algorithm Codes/Graph/AdjacencyMatrixGraph.py
+++ Algorithm Codes/Graph/AdjacencyMatrixGraph.py
@@ -2,15 +2,14 @@ from Graph import *
 import heapq
 
 class AdjacencyMatrixGraph(Graph):
-    def __init__(self, nodes):
+    def __init__(self, nodes, edges=None):
         self.n = nodes
         self.matrix = [[0 for j in range(self.n)] for i in range(self.n)]
-    
-    def __init(self, nodes, edges):
-        self.__init__(nodes)
-        for ((u, v), w) in edges:
-            self.matrix[u][v] = w
-            self.matrix[v][u] = w
+
+        if(edges != None):
+            for ((u, v), w) in edges:
+                self.matrix[u][v] = w
+                self.matrix[v][u] = w
     
     def add_edge(self, edge):
         ((u, v), w) = edge
@@ -22,26 +21,33 @@ class AdjacencyMatrixGraph(Graph):
         self.matrix[u][v] = 0
         self.matrix[v][u] = 0
 
-    def dijkstra(self, start, end):
-        visited = [False for i in range(n)]
-        in_heap = [False for i in range(n)]
+    def dijkstra(self, start):
+        dist = [float('inf') for i in range(self.n)]
+        dist[start] = 0
+        prev = [None for i in range(self.n)]
+        visited = [False for i in range(self.n)]
 
         heap = []
         heapq.heappush(heap, (0, start))
-        in_heap[start] = True
 
         while(heap):
             (path_len, u) = heapq.heappop(heap)
-            in_heap[u] = False
             visited[u] = True
-            if(u == end):
-                return path_len
             for i in range(self.n):
-                if(self.matrix[u][i] > 0 and not(visited[i]) and not(in_heap[i])):
-                    heapq.heappush(heap, (path_len + self.matrix[u][i], i))
-                    in_heap[i] = True
-        
-        return None
+                if(self.matrix[u][i] != 0 and not(visited[i])):
+                    new_path_len = dist[u] + self.matrix[u][i]
+                    if(new_path_len < dist[i]):
+                        dist[i] = new_path_len
+                        prev[i] = u
+                        heapq.heappush(heap, (dist[i], i))
+            
+        return (dist, prev)
+
+nodes = 6
+edges = [((0,1), 3), ((0,2), 8), ((1,2), 5), ((1,3), 6), ((2,3), 3), ((2,4), 2), ((3,4), 1), ((3,5), 9), ((4,5), 3)]
+
+graph = AdjacencyMatrixGraph(nodes, edges)
+print(graph.dijkstra(0))
 
     
 
