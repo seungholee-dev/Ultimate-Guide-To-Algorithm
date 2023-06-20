d423de687ec9d705d2d3881fb365a9ad0d2d3310
Seungho <poream3387@gmail.com>
2023-06-20 16:31:23 +0900

    feature: Modify frontpage

---
+++ website/astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Ultimate Algorithm',
+      social: {
+        github: 'https://github.com/PricelessCode/Ultimate-Guide-To-Algorithm',
+      },
+
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
