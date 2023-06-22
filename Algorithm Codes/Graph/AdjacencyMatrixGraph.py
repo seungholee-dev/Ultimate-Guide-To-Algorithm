731ab87919e07d926cdb3083f076a0fd0bb7d6c4
Nananaw <gamingnananaw@gmail.com>
2023-06-21 22:00:56 -0400

    Updated if statement for is_directed, and fixed big(O) for bellman_ford algorithm

--- Algorithm Codes/Graph/AdjacencyMatrixGraph.py
+++ Algorithm Codes/Graph/AdjacencyMatrixGraph.py
@@ -10,19 +10,19 @@ class AdjacencyMatrixGraph(Graph):
         if(edges != None):
             for ((u, v), w) in edges:
                 self.matrix[u][v] = w
-                if(not(is_directed)):
+                if not self.is_directed:
                     self.matrix[v][u] = w
     
     def add_edge(self, edge):
         ((u, v), w) = edge
         self.matrix[u][v] = w
-        if(not(self.is_directed)):
+        if not self.is_directed:
             self.matrix[v][u] = w
     
     def remove_edge(self, edge):
         (u, v) = edge
         self.matrix[u][v] = 0
-        if(not(self.is_directed)):
+        if not self.is_directed:
             self.matrix[v][u] = w
 
     def dijkstra(self, start):
@@ -52,13 +52,17 @@ class AdjacencyMatrixGraph(Graph):
         dist[start] = 0
         prev = [None for i in range(self.n)]
 
-        for i in range(self.n-1):
+        edges = []
+        for i in range(self.n):
             for j in range(self.n):
-                for k in range(self.n):
-                    if(self.matrix[j][k] != 0):
-                        if(self.matrix[j][k] + dist[j] < dist[k]):
-                            dist[k] = self.matrix[j][k] + dist[j]
-                            prev[k] = j
+                if(self.matrix[i][j] != 0):
+                    edges.append(((i,j), self.matrix[i][j]))
+
+        for i in range(self.n-1):
+            for ((u,v), w) in edges:
+                if(w + dist[u] < dist[v]):
+                    dist[v] = w + dist[u]
+                    prev[v] = u
         
         if(prev[start] != None):
             return None
@@ -73,13 +77,13 @@ graph = AdjacencyMatrixGraph(nodes, edges)
 print(graph.dijkstra(0))
 
 
-#graph3 = AdjacencyMatrixGraph(5, [((0, 1), 4), ((0, 2), 2), ((1, 3), -1), ((2, 3), 3), ((2, 4), 5), ((3, 4), 2)], True)
-#graph4 = AdjacencyMatrixGraph(4, [((0, 1), 3), ((0, 2), 2), ((1, 2), -2), ((1, 3), 4), ((2, 3), 1)], True)
-#graph5 = AdjacencyMatrixGraph(3, [((0, 1), 1), ((1, 2), -1), ((2, 0), -2)], True)
+graph3 = AdjacencyMatrixGraph(5, [((0, 1), 4), ((0, 2), 2), ((1, 3), -1), ((2, 3), 3), ((2, 4), 5), ((3, 4), 2)], True)
+graph4 = AdjacencyMatrixGraph(4, [((0, 1), 3), ((0, 2), 2), ((1, 2), -2), ((1, 3), 4), ((2, 3), 1)], True)
+graph5 = AdjacencyMatrixGraph(3, [((0, 1), 1), ((1, 2), -1), ((2, 0), -2)], True)
 
-#print(graph3.bellman_ford(0))
-#print(graph4.bellman_ford(0))
-#print(graph5.bellman_ford(0))
+print(graph3.bellman_ford(0))
+print(graph4.bellman_ford(0))
+print(graph5.bellman_ford(0))
 
 
     
