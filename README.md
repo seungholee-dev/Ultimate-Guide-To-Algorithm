62512e8c18df8193879cbc0fefba6c1b07688008
Seungho <poream3387@gmail.com>
2023-06-24 01:54:43 +0900

    fix: Add missing dependency

--- README.md
+++ README.md
@@ -32,58 +32,6 @@ Here are some examples of Big O Notation:
 <br>
 <br>
 
-### 2. Time Complexity :timer_clock:
-
-Time Complexity is computational complexity that describes **the amount of computer time it takes to run an algorithm**
-
-Here are some examples of time complexity
-<br>
-
-##### Question 1
-
-```python
-nums = [1, 2, 3, 4, 5]
-for n in nums:
-    print(n)
-```
-
-<details>
-<summary>Answer</summary>
-<div markdown="1">
-<b>O(N)</b>
-<br>
-The code runtime depends on length of nums. As the nums gets bigger, the run time only gets bigger in linear time O(N)
-</div>
-</details>
-
-<br>
-
-##### Question 2
-
-```python
-nums = [1, 2, 3, 4, 5]
-
-for n1 in nums:
-    for n2 in nums:
-        print(n1, n2)
-```
-
-<details>
-<summary>Answer</summary>
-<div markdown="1">
-<b>O(N^2)</b>
-<br>
-The code runtime depends on length of nums. As the nums gets bigger, the run time gets bigger by N^2 because of the nested loop
-</div>
-</details>
-<br>
-
-#### Amortized vs Averaged Runtime?
-
--   Averaged is often used when the case is random(unpredictable).
--   Amortized is often used when the case is predictable but is too good to just diminish all the runtime performance just because of some slow cases(predictable).
-
--   From: Amortized vs Averaged runtime https://gist.github.com/jconnolly/5acf05f279a7e9e40371
 
 <br>
 <br>
--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,34 +1,38 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        starlight({
-            title: "Ultimate Algorithm",
-            social: {
-                github: "https://github.com/PricelessCode/Ultimate-Guide-To-Algorithm",
-            },
-
-            sidebar: [
-                {
-                    label: "Basics",
-                    items: [
-                        { label: "Big O Notation", link: "/basics/big-o-notation" },
-                        { label: "Time Complexity", link: "/basics/time-complexity" },
-                        { label: "Space Complexity", link: "/basics/space-complexity" },
-                    ],
-                },
-                {
-                    label: "Sorting",
-                    items: [
-                      { label: "Bubble Sort", link: "/sorting/bubble-sort" },
-                    ]
-                },
-            ],
-        }),
-    ],
-
-    // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-    image: { service: { entrypoint: "astro/assets/services/sharp" } },
+  integrations: [starlight({
+    title: "Ultimate Algorithm",
+    social: {
+      github: "https://github.com/PricelessCode/Ultimate-Guide-To-Algorithm"
+    },
+    sidebar: [{
+      label: "Basics",
+      items: [{
+        label: "Big O Notation",
+        link: "/basics/big-o-notation"
+      }, {
+        label: "Time Complexity",
+        link: "/basics/time-complexity"
+      }, {
+        label: "Space Complexity",
+        link: "/basics/space-complexity"
+      }]
+    }, {
+      label: "Sorting",
+      items: [{
+        label: "Bubble Sort",
+        link: "/sorting/bubble-sort"
+      }]
+    }]
+  })],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp"
+    }
+  }
 });
