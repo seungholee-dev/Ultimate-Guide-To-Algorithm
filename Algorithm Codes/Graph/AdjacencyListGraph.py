1eadbea92ffaa45c405a446419fd95608331ca94
Nathan Neequaye <44145746+Nananaw@users.noreply.github.com>
2023-07-10 01:23:25 -0400

    Implemented Bellman-Ford algorithm and Floyd-Warshall algorithm for AdjecencyListGraph (#24)
    * Implemented Bellman-Ford algorithm and Floyd-Warshall algorithm for AdjacencyListGraph
    
    * changed (== None) and (!= None) to (is None) and (is not None)
    
    * parse: removing some parenthesis based on PEP-8
    
    * Update AdjacencyMatrixGraph.py
    
    ---------
    
    Co-authored-by: Seungho Lee <poream3387@gmail.com>

--- Algorithm Codes/Graph/AdjacencyListGraph.py
+++ Algorithm Codes/Graph/AdjacencyListGraph.py
@@ -12,7 +12,7 @@ class LinkedList:
         self.head = None
 
     def add(self, node, weight):
-        if(self.head == None):
+        if(self.head is None):
             self.head = ListNode(node, weight)
         else:
             new_head = ListNode(node, weight)
@@ -26,7 +26,7 @@ class LinkedList:
         prev = dummy
         curr = self.head
 
-        while(curr != None):
+        while(curr is not None):
             if(curr.node == node):
                 prev.next = curr.next
                 break
@@ -38,10 +38,11 @@ class LinkedList:
     def to_list(self):
         res = []
         curr = self.head
-        while(curr != None):
+        while(curr is not None):
             res.append((curr.node, curr.weight))
             curr = curr.next
         return res
+        
 '''
 a = LinkedList()
 print(a.to_list())
@@ -99,7 +100,7 @@ class AdjacencyListGraph(Graph):
             visited[u] = True
 
             curr = self.adj[u].head
-            while(curr != None):
+            while(curr is not None):
                 new_path_len = dist[u] + curr.weight
                 if(new_path_len < dist[curr.node]):
                     dist[curr.node] = new_path_len
@@ -108,10 +109,62 @@ class AdjacencyListGraph(Graph):
                 curr = curr.next
 
         return (dist, prev)
+    
+    def bellman_ford(self, start):
+        dist = [float('inf') for i in range(self.n)]
+        dist[start] = 0
+        prev = [None for i in range(self.n)]
+
+        for i in range(self.n-1):
+            for u in range(self.n):
+                edge = self.adj[u].head
+                while(edge is not None):
+                    if(edge.weight + dist[u] < dist[edge.node]):
+                        dist[edge.node] = edge.weight + dist[u]
+                        prev[edge.node] = u
+                    edge = edge.next
+        
+        if(prev[start] is not None):
+            return None
+        
+        return (dist, prev)
+    
+    def floyd_warshall(self):
+        dist = [[float('inf') for i in range(self.n)] for j in range(self.n)]
+
+        for i in range(self.n):
+            dist[i][i] = 0
+            edge = self.adj[i].head
+            while(edge is not None):
+                dist[i][edge.node] = edge.weight
+                edge = edge.next
+        
+        for k in range(self.n):
+            for u in range(self.n):
+                for v in range(self.n):
+                    if(dist[u][v] > dist[u][k] + dist[k][v]):
+                        dist[u][v] = dist[u][k] + dist[k][v]            
+        
+        for i in range(self.n):
+            if(dist[i][i] < 0):
+                return None
+        
+        return dist
 
 nodes = 6
 edges = [((0,1), 3), ((0,2), 8), ((1,2), 5), ((1,3), 6), ((2,3), 3), ((2,4), 2), ((3,4), 1), ((3,5), 9), ((4,5), 3)]
 
 graph = AdjacencyListGraph(nodes, edges)
 print(graph.dijkstra(0))
+
+graph3 = AdjacencyListGraph(5, [((0, 1), 4), ((0, 2), 2), ((1, 3), -1), ((2, 3), 3), ((2, 4), 5), ((3, 4), 2)], True)
+graph4 = AdjacencyListGraph(4, [((0, 1), 3), ((0, 2), 2), ((1, 2), -2), ((1, 3), 4), ((2, 3), 1)], True)
+graph5 = AdjacencyListGraph(3, [((0, 1), 1), ((1, 2), -1), ((2, 0), -2)], True)
+
+print(graph3.bellman_ford(0))
+print(graph3.floyd_warshall()[0])
+print(graph4.bellman_ford(0))
+print(graph4.floyd_warshall()[0])
+print(graph5.bellman_ford(0))
+print(graph5.floyd_warshall())
         
--- Algorithm Codes/Graph/AdjacencyMatrixGraph.py
+++ Algorithm Codes/Graph/AdjacencyMatrixGraph.py
@@ -4,7 +4,7 @@ import heapq
 class AdjacencyMatrixGraph(Graph):
     def __init__(self, nodes, edges=[], is_directed=False):
         self.n = nodes
-        self.matrix = [[0 for j in range(self.n)] for i in range(self.n)]
+        self.matrix = [[0 for _ in range(self.n)] for _ in range(self.n)]
         self.is_directed = is_directed
 
         for ((u, v), w) in edges:
@@ -13,78 +13,79 @@ class AdjacencyMatrixGraph(Graph):
                 self.matrix[v][u] = w
     
     def add_edge(self, edge):
-        ((u, v), w) = edge
+        (u, v), w = edge
         self.matrix[u][v] = w
         if not self.is_directed:
             self.matrix[v][u] = w
     
     def remove_edge(self, edge):
-        ((u, v), _ ) = edge
+        (u, v), _ = edge
         self.matrix[u][v] = 0
         if not self.is_directed:
             self.matrix[v][u] = w
 
     def dijkstra(self, start):
-        dist = [float('inf') for i in range(self.n)]
+        dist = [float('inf') for _ in range(self.n)]
         dist[start] = 0
-        prev = [None for i in range(self.n)]
-        visited = [False for i in range(self.n)]
+        prev = [None for _ in range(self.n)]
+        visited = [False for _ in range(self.n)]
 
         heap = []
         heapq.heappush(heap, (0, start))
 
-        while(heap):
-            (path_len, u) = heapq.heappop(heap)
+        while heap:
+            path_len, u = heapq.heappop(heap)
             visited[u] = True
             for i in range(self.n):
-                if(self.matrix[u][i] != 0 and not(visited[i])):
+                if self.matrix[u][i] and not visited[i]:
                     new_path_len = dist[u] + self.matrix[u][i]
-                    if(new_path_len < dist[i]):
+                    if new_path_len < dist[i]:
                         dist[i] = new_path_len
                         prev[i] = u
                         heapq.heappush(heap, (dist[i], i))
             
-        return (dist, prev)
+        return dist, prev
     
     def bellman_ford(self, start):
-        dist = [float('inf') for i in range(self.n)]
+        dist = [float('inf') for _ in range(self.n)]
         dist[start] = 0
-        prev = [None for i in range(self.n)]
+        prev = [None for _ in range(self.n)]
 
         edges = []
+        
         for i in range(self.n):
             for j in range(self.n):
-                if(self.matrix[i][j] != 0):
-                    edges.append(((i,j), self.matrix[i][j]))
+                if self.matrix[i][j]:
+                    edges.append(((i, j), self.matrix[i][j]))
 
-        for i in range(self.n-1):
+        for _ in range(self.n - 1):
             for ((u,v), w) in edges:
-                if(w + dist[u] < dist[v]):
+                if w + dist[u] < dist[v]:
                     dist[v] = w + dist[u]
                     prev[v] = u
         
-        if(prev[start] != None):
+        if prev[start] is not None:
             return None
         
         return (dist, prev)
 
     def floyd_warshall(self):
-        dist = [[float('inf') for i in range(self.n)] for j in range(self.n)]
+        dist = [[float('inf') for _ in range(self.n)] for _ in range(self.n)]
 
         for i in range(self.n):
             dist[i][i] = 0
             for j in range(self.n):
-                if(self.matrix[i][j] != 0):
+                if self.matrix[i][j] != 0:
                     dist[i][j] = self.matrix[i][j]
             
         for k in range(self.n):
             for u in range(self.n):
                 for v in range(self.n):
-                    if(dist[u][v] > dist[u][k] + dist[k][v]):
+                    if dist[u][v] > dist[u][k] + dist[k][v]:
                         dist[u][v] = dist[u][k] + dist[k][v]
         
         for i in range(self.n):
-            if(dist[i][i] < 0):
+            if dist[i][i] < 0:
                 return None
             
         return dist
