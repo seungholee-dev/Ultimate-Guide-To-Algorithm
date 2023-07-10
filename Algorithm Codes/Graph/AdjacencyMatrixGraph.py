791a17fd09f98fd1bd12e826954ddca2e5293be5
Seungho Lee <poream3387@gmail.com>
2023-07-10 10:55:45 +0900

    Update AdjacencyMatrixGraph.py

--- Algorithm Codes/Graph/AdjacencyMatrixGraph.py
+++ Algorithm Codes/Graph/AdjacencyMatrixGraph.py
@@ -4,7 +4,7 @@ import heapq
 class AdjacencyMatrixGraph(Graph):
     def __init__(self, nodes, edges=[], is_directed=False):
         self.n = nodes
-        self.matrix = [[0 for j in range(self.n)] for i in range(self.n)]
+        self.matrix = [[0 for _ in range(self.n)] for _ in range(self.n)]
         self.is_directed = is_directed
 
         for ((u, v), w) in edges:
@@ -13,51 +13,52 @@ class AdjacencyMatrixGraph(Graph):
                 self.matrix[v][u] = w
     
     def add_edge(self, edge):
-        ((u, v), w) = edge
+        (u, v), w = edge
         self.matrix[u][v] = w
         if not self.is_directed:
             self.matrix[v][u] = w
     
     def remove_edge(self, edge):
-        ((u, v), _ ) = edge
+        (u, v), _ = edge
         self.matrix[u][v] = 0
         if not self.is_directed:
             self.matrix[v][u] = w
 
     def dijkstra(self, start):
-        dist = [float('inf') for i in range(self.n)]
+        dist = [float('inf') for _ in range(self.n)]
         dist[start] = 0
-        prev = [None for i in range(self.n)]
-        visited = [False for i in range(self.n)]
+        prev = [None for _ in range(self.n)]
+        visited = [False for _ in range(self.n)]
 
         heap = []
         heapq.heappush(heap, (0, start))
 
         while heap:
-            (path_len, u) = heapq.heappop(heap)
+            path_len, u = heapq.heappop(heap)
             visited[u] = True
             for i in range(self.n):
-                if self.matrix[u][i] != 0 and not(visited[i]):
+                if self.matrix[u][i] and not visited[i]:
                     new_path_len = dist[u] + self.matrix[u][i]
                     if new_path_len < dist[i]:
                         dist[i] = new_path_len
                         prev[i] = u
                         heapq.heappush(heap, (dist[i], i))
             
-        return (dist, prev)
+        return dist, prev
     
     def bellman_ford(self, start):
-        dist = [float('inf') for i in range(self.n)]
+        dist = [float('inf') for _ in range(self.n)]
         dist[start] = 0
-        prev = [None for i in range(self.n)]
+        prev = [None for _ in range(self.n)]
 
         edges = []
+        
         for i in range(self.n):
             for j in range(self.n):
                 if self.matrix[i][j]:
-                    edges.append(((i,j), self.matrix[i][j]))
+                    edges.append(((i, j), self.matrix[i][j]))
 
-        for i in range(self.n-1):
+        for _ in range(self.n - 1):
             for ((u,v), w) in edges:
                 if w + dist[u] < dist[v]:
                     dist[v] = w + dist[u]
@@ -69,7 +70,7 @@ class AdjacencyMatrixGraph(Graph):
         return (dist, prev)
 
     def floyd_warshall(self):
-        dist = [[float('inf') for i in range(self.n)] for j in range(self.n)]
+        dist = [[float('inf') for _ in range(self.n)] for _ in range(self.n)]
 
         for i in range(self.n):
             dist[i][i] = 0
